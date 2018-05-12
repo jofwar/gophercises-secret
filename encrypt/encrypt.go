06f2a5d029be4c96ee357909d328ef6e1d31fffb
jofwar <jason.hutchings.code@gmail.com>
2018-05-12 23:31:57 +0100

    Started on decrypt function

--- encrypt/encrypt.go
+++ encrypt/encrypt.go
@@ -46,6 +46,31 @@ func Encrypt(key []byte, value string) (cipherstring, auth string, err error) {
 	return string(ciphertext), string(expectedMAC), nil
 }
 
-func decrypt(key, cipher string) string {
+func Decrypt(key []byte, ciphertext string) string {
+	// Load your secret key from a safe place and reuse it across multiple
+	// NewCipher calls. (Obviously don't use this example key for anything
+	// real.) If you want to convert a passphrase to a key, use a suitable
+	// package like bcrypt or scrypt.
+	//key, _ := hex.DecodeString("6368616e676520746869732070617373")
+	//ciphertext, _ := hex.DecodeString("7dd015f06bec7f1b8f6559dad89f4131da62261786845100056b353194ad")
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := []byte(ciphertext[:aes.BlockSize])
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream([]byte(ciphertext), []byte(ciphertext))
+
 	return ""
 }
